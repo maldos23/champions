1900c169e37bce2ab837baae8c3a1f573f02fa0c
Gino-Kairos <103150718+Gino-Kairos@users.noreply.github.com>
2022-07-14 16:06:58 -0500

    init: First commit

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+// pages/_app.js
+import { ChakraProvider } from "@chakra-ui/react";
+
+// 1. Import the extendTheme function
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+function Champions({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default Champions;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Container, Text } from "@chakra-ui/react";
+
+export default function Init() {
+  return (
+    <div>
+      <Container maxW="xl">
+        <Text fontSize="6xl">Aqui estara la champions</Text>
+      </Container>
+    </div>
+  );
+}
